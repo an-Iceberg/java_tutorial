988dd703c990212ae7687093e115b79abfda6aab
an-Iceberg <a.g.marosi@bluewin.ch>
2022-11-25 22:23:06 +0100

    done restructuring

--- src/Functions.java
+++ src/Functions.java
@@ -3,5 +3,28 @@ package src;
 public class Functions {
   public static void run() {
     System.out.println("  Functions".toUpperCase());
+
+    // Functions
+    boolean someBool = someFuncion();
+
+    System.out.println(someFuncion());
+
+    // Scope (variable scope)
+    {
+      int my_number = 3434;
+      System.out.println("my_number in first scope:" + my_number);
+    }
+
+    {
+      int my_number = 19191;
+      System.out.println("my_number in second scope:" + my_number);
+    }
+
+  }
+
+  // Function declaration
+  public static boolean someFuncion() {
+    /* Function body */
+    return false;
   }
 }
